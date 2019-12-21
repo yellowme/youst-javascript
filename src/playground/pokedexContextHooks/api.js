b1f62ad812bf965b77bb99e1bba5896936b3d4d3
jerolan <jerome.olvera@gmail.com>
2019-12-21 15:34:36 -0600

    Add HTTP client

--- src/playground/pokedexContextHooks/api.js
+++ src/playground/pokedexContextHooks/api.js
@@ -1,13 +1,25 @@
+async function apiClient({ path = "/", method = "GET", body }) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon${path}`, {
+    method: method,
+    ...(body ? { body: JSON.stringify(body) } : {}),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (!response.ok) throw response;
+  const json = response.json();
+  return json;
+}
+
 async function allPokemons() {
-  const response = await fetch("http://pokeapi.co/api/v2/pokemon?limit=151");
-  const json = await response.json();
-  return json.results;
+  const response = await apiClient({ path: "?limit=151" });
+  return response.results;
 }
 
 async function pokemon(id) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  const json = await response.json();
-  return json;
+  const response = await apiClient({ path: `/${id}` });
+  return response;
 }
 
 export default { allPokemons, pokemon };
